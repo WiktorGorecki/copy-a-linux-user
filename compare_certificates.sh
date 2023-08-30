8d95f133e163467b90978bc43f9dd3f1a16e9cdb
WiktorGorecki <60294931+WiktorGorecki@users.noreply.github.com>
2023-08-30 10:01:33 +0200

    Create compare_certificates.sh

---
+++ compare_certificates.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+regex="*.pem" # Name of the certificate file
+path="."      # Path to the certificate
+
+echo "Looking for files"
+files=`find $path -name $regex`
+
+# Uncomment for debug or to get a string with all the files' paths
+# echo "Theese files were found:"
+# echo $files
+
+for file in $files
+do
+echo "Result of md5sum $file"
+echo `md5sum "$file"`
+echo ""
+done
